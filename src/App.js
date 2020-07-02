024c257f4dba050e594a1058830fe84183c39ee4
Anurag <anurag@storeking.in>
2020-07-02 22:08:03 +0530

    Implement Login Screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import Login from './Components/Login';
+
+const initialState = {
+  loggedIn: false,
+
+}
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  doLogin() {
+    this.setState({
+      loggedIn: true
+    })
+  }
+
+  getHomeScreen() {
+    return (
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Loign Success
       </header>
-    </div>
-  );
+    )
+  }
+
+  getLoginScreen() {
+    return (
+      <header className="App-header">
+        <Login login={this.doLogin.bind(this)} />
+      </header>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <header className="App-header"> */}
+        {this.state.loggedIn ? this.getHomeScreen() : this.getLoginScreen()}
+        {/* </header> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Login.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+
+const clientId = "617492017458-5gbk6gu4kffbiti0m86akvgpdmo7ukj6.apps.googleusercontent.com";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onLogin() {
+    this.props.login();
+  }
+
+  onLoginError() {
+    alert("Login Failed");
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        <div className="LoginModal">
+          <GoogleLogin
+            clientId={clientId}
+            buttonText="Login using Gmail"
+            onSuccess={this.onLogin.bind(this)}
+            onFailure={this.onLoginError}
+            cookiePolicy={'single_host_origin'}
+          />
+        </div>
+      </div>
+    )
+  }
+
+}
+
+
+export default Login;
